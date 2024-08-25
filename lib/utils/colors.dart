9948bbb3e41356d601abffaa860c0c6665dfcc26
KamelKOFASH <mk8764687@gmail.com>
2024-08-25 21:23:19 +0300

    final without reminder

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color primaryColor = Color(0xff11212D);
+  static const List<Color> primaryGradientColor = [
+    Color(0xff253745),
+    Color(0xff9BA8AB),
+  ];
+}
